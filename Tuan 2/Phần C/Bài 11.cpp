758874ed69eb5c678f1688c2bd4c43a033ed1da8
phanhienan <78184196+phanhienan@users.noreply.github.com>
2021-02-25 21:28:48 +0700

    Week2.sectionC

--- Tuan 2/Phần C/Bài 11.cpp
+++ Tuan 2/Phần C/Bài 11.cpp
@@ -10,7 +10,7 @@ int main()
         double doC, doK;
         doC = (i*1.0 - 32)*5/9;
         doK = doC + 273.15;
-        cout << fixed << setprecision(2) <<  i << " " << doC << " " << doK << endl;
+        cout << fixed << setprecision(2) <<  i << "\t" << doC << "\t" << doK << endl;
     }
     return 0;
 }
